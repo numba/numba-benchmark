e87490ea157f4882f644329e4b447f51c0a2acb3
Antoine Pitrou <antoine@python.org>
2014-12-10 19:56:47 +0100

    Add a relative difference vectorization benchmark

--- benchmarks/bench_vectorize.py
+++ benchmarks/bench_vectorize.py
@@ -15,6 +15,14 @@ def mul(x, y):
     return x * y
 
 
+@vectorize(["float32(float32, float32)",
+            "float64(float64, float64)"])
+def rel_diff(x, y):
+    # XXX for float32 performance, we should write `np.float32(2)`, but
+    # that's not the natural way to write this code...
+    return 2 * (x - y) / (x + y)
+
+
 class TimeSuite:
 
     n = 10000
@@ -24,7 +32,7 @@ class TimeSuite:
         self.samples = {}
         self.out = {}
         for dtype in self.dtypes:
-            self.samples[dtype] = np.linspace(0, 1, self.n, dtype=dtype)
+            self.samples[dtype] = np.linspace(0.1, 1, self.n, dtype=dtype)
             self.out[dtype] = np.zeros(self.n, dtype=dtype)
 
     def _binary_func(func, dtype):
@@ -34,5 +42,8 @@ class TimeSuite:
 
     for dtype in dtypes:
         locals()['time_mul_%s' % dtype] = _binary_func(mul, dtype)
+
+    time_rel_diff_float32 = _binary_func(rel_diff, 'float32')
+    time_rel_diff_float64 = _binary_func(rel_diff, 'float64')
 
     del _binary_func
